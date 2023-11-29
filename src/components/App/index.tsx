4cad7f26e6b90c71f58d228d6230f1673bca4802
Greyson Elkins <greysonelkins@gmail.com>
2023-11-29 00:36:06 -0700

    Add under construction

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -5,6 +5,9 @@ import './App.scss'
 const App = () => 
   <main className="App">
     <Header/>
+    <div className="in-progress">
+      <span>YEAH Yeah yeah</span>, I know - I'm working on it
+    </div>
   </main>
 
 export default App;
