bc6466f96d259ab5789805f8824b2ae9a0884e04
Greyson Elkins <greysonelkins@gmail.com>
2023-12-01 13:52:27 -0700

    Add web-app-capable meta tags

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,5 +1,3 @@
-import { useEffect } from 'react';
-
 import HeaderFooter from 'components/HeaderFooter'
 import PlayerContext from 'context/Player'
 import Music from 'views/music';
@@ -7,22 +5,18 @@ import Music from 'views/music';
 import './App.scss'
 import Code from 'views/code';
 
-const App = () => {
-  
-  useEffect(() => window.scrollTo(0,0), [])
-  
-  return (
-    <div className="App">
-      <PlayerContext>
-        <HeaderFooter>
-          <main>
-            <Code />
-            <Music />
-          </main>
-        </HeaderFooter>
-      </PlayerContext>
-    </div>
-  )
-}
+const App = () => (
+  <div className="App">
+    <PlayerContext>
+      <HeaderFooter>
+        <main>
+          <Code />
+          <Music />
+        </main>
+      </HeaderFooter>
+    </PlayerContext>
+  </div>
+)
+
 
 export default App;
