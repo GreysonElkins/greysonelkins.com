03d1aaa802de79c84507547f9775a3b9203c14f5
Greyson Elkins <greysonelkins@gmail.com>
2023-12-01 13:49:05 -0700

    Add scroll to

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import HeaderFooter from 'components/HeaderFooter'
 import PlayerContext from 'context/Player'
 import Music from 'views/music';
@@ -5,16 +7,22 @@ import Music from 'views/music';
 import './App.scss'
 import Code from 'views/code';
 
-const App = () => 
-  <div className="App">
-    <PlayerContext>
-      <HeaderFooter>
-        <main>
-          <Code />
-          <Music />
-        </main>
-      </HeaderFooter>
-    </PlayerContext>
-  </div>
+const App = () => {
+  
+  useEffect(() => window.scrollTo(0,0), [])
+  
+  return (
+    <div className="App">
+      <PlayerContext>
+        <HeaderFooter>
+          <main>
+            <Code />
+            <Music />
+          </main>
+        </HeaderFooter>
+      </PlayerContext>
+    </div>
+  )
+}
 
 export default App;
