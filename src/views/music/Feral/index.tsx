2b25b2fcd5bd0b0597875d9591837875778c66d4
Greyson Elkins <greysonelkins@gmail.com>
2023-12-02 17:30:13 -0700

    Add band socials

--- src/views/music/Feral/index.tsx
+++ src/views/music/Feral/index.tsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpotify, faInstagram } from '@fortawesome/free-brands-svg-icons'
+
 import feral from '../../../assets/feral.png'
 import useView from 'hooks/useView'
 
@@ -5,15 +8,31 @@ import './Feral.scss'
 
 const Feral = () => {
   const { isOnPage } = useView('/music')
-  const open = isOnPage ? "open" : "closed"
+  const open = isOnPage ? 'open' : 'closed'
 
-  return <section className={`Feral band ${open}`}>
-    <h2>singer for <span>Feral Suits</span></h2>
-    <div className="body">
+  return (
+    <section className={`Feral band ${open}`}>
+      <h2>
+        singer for <span>Feral Suits</span>
+      </h2>
+      <div className="socials">
+        <a href="https://www.instagram.com/feralsuits" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faInstagram} size={'lg'} />
+        </a>
+        <a
+          href="https://open.spotify.com/artist/3NS8aEjC3lH0xDynDB7zZK?si=a5h05WpgTWKmSaOGD4D4_Q"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon icon={faSpotify} size={'lg'} />
+        </a>
+      </div>
+      <div className="body">
         <img src={feral} alt="Five musicians, a rock band" />
         <div>Feral Suits is another band, etc.</div>
-    </div>
-  </section>
+      </div>
+    </section>
+  )
 }
 
 export default Feral
--- src/views/music/Snarklet/index.tsx
+++ src/views/music/Snarklet/index.tsx
@@ -1,20 +1,39 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpotify, faInstagram } from '@fortawesome/free-brands-svg-icons'
+
 import useView from 'hooks/useView'
 import snarklet from '../../../assets/snark.png'
 
 import './Snarklet.scss'
 
 const Snarklet: React.FC = () => {
-    const { isOnPage } = useView('/music')
+  const { isOnPage } = useView('/music')
 
-    const open = isOnPage ? "open" : "closed"
+  const open = isOnPage ? 'open' : 'closed'
 
-    return <section className={`Snarklet band ${open}`}>
-        <h2>guitar for <span>Snarklet</span></h2>
-        <div className="body">
-          <img src={snarklet} alt="Snarklet the singer / Greyson on guitar"/>
-          <div>Snarklet is a band, etc.</div>
-        </div>
-  </section>
+  return (
+    <section className={`Snarklet band ${open}`}>
+      <h2>
+        guitar for <span>Snarklet</span>
+      </h2>
+      <div className="socials">
+        <a href="https://www.instagram.com/snarklet" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faInstagram} size={'lg'} />
+        </a>
+        <a
+          href="https://open.spotify.com/artist/1l9LXROEFaVOafZg3GkA4Y?si=i4OTrU9nQ5CsUq00ntab0A"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon icon={faSpotify} size={'lg'} />
+        </a>
+      </div>
+      <div className="body">
+        <img src={snarklet} alt="Snarklet the singer / Greyson on guitar" />
+        <div>Snarklet is a band, etc.</div>
+      </div>
+    </section>
+  )
 }
 
 export default Snarklet
