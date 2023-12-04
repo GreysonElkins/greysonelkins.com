c8afc90319d12c1502482f9fbeea7036c4095852
Greyson Elkins <greysonelkins@gmail.com>
2023-12-04 12:42:16 -0700

    Add band descriptions

--- src/views/music/Feral/index.tsx
+++ src/views/music/Feral/index.tsx
@@ -14,22 +14,34 @@ const Feral = () => {
     <section className={`Feral band ${open}`}>
       <h2>
         singer for <span>Feral Suits</span>
+        <div className="socials">
+          <a
+            href="https://open.spotify.com/artist/3NS8aEjC3lH0xDynDB7zZK?si=a5h05WpgTWKmSaOGD4D4_Q"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faSpotify} size={'lg'} />
+          </a>
+          <a href="https://www.instagram.com/feralsuits" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faInstagram} size={'lg'} />
+          </a>
+        </div>
       </h2>
-      <div className="socials">
-        <a href="https://www.instagram.com/feralsuits" target="_blank" rel="noreferrer">
-          <FontAwesomeIcon icon={faInstagram} size={'lg'} />
-        </a>
-        <a
-          href="https://open.spotify.com/artist/3NS8aEjC3lH0xDynDB7zZK?si=a5h05WpgTWKmSaOGD4D4_Q"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FontAwesomeIcon icon={faSpotify} size={'lg'} />
-        </a>
-      </div>
       <div className="body">
         <img src={feral} alt="Five musicians, a rock band" />
-        <div>Feral Suits is another band, etc.</div>
+        <div className="description">
+          Feral Suits is a Denver Colorado Super Group founded by{' '}
+          <a
+            href="https://open.spotify.com/artist/0vWfV0vB0moYPgiMxFpJoM?si=9hJVaMBNTgSYo2IPva693Q"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Hot Cold Ghost
+          </a>{' '}
+          in 2018. We released our first record, Drown the Garden, in 2022, which had singles
+          featured in editorial playlists by Spotify and Amuse. I play a little guitar, but mostly
+          I'm waxing poetic and doing subtle impressions of all my favorite singers.
+        </div>
       </div>
     </section>
   )
--- src/views/music/Snarklet/index.tsx
+++ src/views/music/Snarklet/index.tsx
@@ -15,22 +15,27 @@ const Snarklet: React.FC = () => {
     <section className={`Snarklet band ${open}`}>
       <h2>
         guitar for <span>Snarklet</span>
+        <div className="socials">
+          <a href="https://www.instagram.com/snarklet" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faInstagram} size={'lg'} />
+          </a>
+          <a
+            href="https://open.spotify.com/artist/1l9LXROEFaVOafZg3GkA4Y?si=i4OTrU9nQ5CsUq00ntab0A"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faSpotify} size={'lg'} />
+          </a>
+        </div>
       </h2>
-      <div className="socials">
-        <a href="https://www.instagram.com/snarklet" target="_blank" rel="noreferrer">
-          <FontAwesomeIcon icon={faInstagram} size={'lg'} />
-        </a>
-        <a
-          href="https://open.spotify.com/artist/1l9LXROEFaVOafZg3GkA4Y?si=i4OTrU9nQ5CsUq00ntab0A"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FontAwesomeIcon icon={faSpotify} size={'lg'} />
-        </a>
-      </div>
       <div className="body">
         <img src={snarklet} alt="Snarklet the singer / Greyson on guitar" />
-        <div>Snarklet is a band, etc.</div>
+        <div className="description">
+          Snarklet is an extra-terrestrial singer and an extra-terrestrial band that's been making
+          indie-pop-jazz music on planet Earth since 2022. We're currently recording our first set
+          of tunes in my home studio. I play a Mexican Fender Jaguar through a self-programmed MIDI
+          capable hand-wired analog pedal board.
+        </div>
       </div>
     </section>
   )
